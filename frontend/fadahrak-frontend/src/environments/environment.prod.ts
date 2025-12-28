fb88e289b21f56ef4dc029a9b165d7aa1c5b6ab6
Abdulrahman-coder32 <g5rys27ff5@privaterelay.appleid.com>
2025-12-28 11:17:20 +0200

    Update environment.prod.ts

--- frontend/fadahrak-frontend/src/environments/environment.prod.ts
+++ frontend/fadahrak-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
-// src/environments/environment.prod.ts
 export const environment = {
-  production: true,
-  API_URL: 'https://your-production-api.com/api' // ضع رابط الباكند النهائي
+  production: true
 };
