0a279f0e281cc7a751f1b5d2bdff57d3d5c4c34a
Abdulrahman-coder32 <g5rys27ff5@privaterelay.appleid.com>
2025-12-31 17:43:13 +0200

    Update job-form.component.ts

--- frontend/fadahrak-frontend/src/app/components/job-form/job-form.component.ts
+++ frontend/fadahrak-frontend/src/app/components/job-form/job-form.component.ts
@@ -146,7 +146,7 @@ import { ApiService } from '../../services/api.service';
 
     .form-input {
       width: 100%;
-      padding: 1rem 1rem 1rem 3rem;
+      padding: 1rem 3rem 1rem 3rem;
       border-radius: 1rem;
       border: 1px solid #D1D5DB;
       background: #FFFFFF;
