7e8ae463d3f1174dd592e510d5c4da5898f718c2
Abdulrahman-coder32 <g5rys27ff5@privaterelay.appleid.com>
2026-01-01 18:26:26 +0200

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['shop_owner', 'job_seeker', 'admin'], required: true },
+  role: { 
+    type: String, 
+    enum: ['shop_owner', 'job_seeker', 'admin'], 
+    required: true 
+  },
   name: String,
   age: Number,
   governorate: { type: String, required: true },
@@ -15,7 +19,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: 'https://res.cloudinary.com/dv48puhaq/image/upload/v1767035882/photo_2025-12-29_21-17-37_irc9se.jpg'
   },
-  cacheBuster: { type: Number, default: 0 }, // جديد: لكسر الكاش في الفرونت
+  cacheBuster: { 
+    type: Number, 
+    default: 0 
+  }, // مهم جدًا: يتزايد كل ما تتغير الصورة عشان نكسر كاش المتصفح والـ CDN
   createdAt: { type: Date, default: Date.now }
 });
 
